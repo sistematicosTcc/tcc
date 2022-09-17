10605076b9f1f5824d2dd722e3566f4fa0d3efc8
Pedro <sistematicostcc@gmail.com>
2022-09-17 00:12:38 -0300

    wallet finalizada

--- front-end/src/component/wallet/walletList.js
+++ front-end/src/component/wallet/walletList.js
@@ -55,62 +55,375 @@ export const WalletList = () => {
   };
 
   const CriarLista = () => {
-    const divMoedas = document.querySelector(".walletBorder");
-    const divList = document.createElement("div");
 
-    const divSeparadores = document.createElement("div");
+    const select = document.getElementById("selecionarMoeda");
+    const opcaoTexto = select.options[select.selectedIndex].text;
 
-    const ul = document.createElement("ul");
-    const li = document.createElement("li");
+    if(opcaoTexto === 'BitCoin'){
+      const divMoedas = document.querySelector(".walletBorder");
+      const divList = document.createElement("div");
 
-    const li2 = document.createElement("li");
-    const divSeparadores2 = document.createElement("div");
-    const ul2 = document.createElement("ul");
+      const divSeparadores = document.createElement("div");
 
-    const li3 = document.createElement("li");
-    const divSeparadores3 = document.createElement("div");
-    const ul3 = document.createElement("ul");
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
 
-    const li4 = document.createElement("li");
-    const divSeparadores4 = document.createElement("div");
-    const ul4 = document.createElement("ul");
+      const li2 = document.createElement("li");
+      const divSeparadores2 = document.createElement("div");
+      const ul2 = document.createElement("ul");
+
+      const li3 = document.createElement("li");
+      const divSeparadores3 = document.createElement("div");
+      const ul3 = document.createElement("ul");
+
+      const li4 = document.createElement("li");
+      const divSeparadores4 = document.createElement("div");
+      const ul4 = document.createElement("ul");
 
 
-    divMoedas.appendChild(divList);
+      divMoedas.appendChild(divList);
 
-    divList.className = 'listaMoedas'
-    divList.appendChild(divSeparadores);
-    divList.appendChild(divSeparadores2);
-    divList.appendChild(divSeparadores3);
-    divList.appendChild(divSeparadores4);
+      divList.className = 'listaMoedas'
+      divList.appendChild(divSeparadores);
+      divList.appendChild(divSeparadores2);
+      divList.appendChild(divSeparadores3);
+      divList.appendChild(divSeparadores4);
 
-    divSeparadores.classList = 'walletMoedas-separadores borderRight'
-    divSeparadores2.classList = 'walletMoedas-separadores borderRight'
-    divSeparadores3.classList = 'walletMoedas-separadores borderRight'
-    divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores4.classList = 'walletMoedas-separadores borderRight'
 
-    divSeparadores.appendChild(ul);
-    divSeparadores2.appendChild(ul2);
-    divSeparadores3.appendChild(ul3);
-    divSeparadores4.appendChild(ul4);
+      divSeparadores.appendChild(ul);
+      divSeparadores2.appendChild(ul2);
+      divSeparadores3.appendChild(ul3);
+      divSeparadores4.appendChild(ul4);
 
-    ul.className = 'walletColumn'
-    
-    ul.appendChild(li);
-    ul2.appendChild(li2);
-    ul3.appendChild(li3);
-    ul4.appendChild(li4);
+      ul.className = 'walletColumn'
+      
+      ul.appendChild(li);
+      ul2.appendChild(li2);
+      ul3.appendChild(li3);
+      ul4.appendChild(li4);
 
-    li.innerHTML = 'Bitcoin'
-    li2.innerHTML = '10'
-    li3.innerHTML = 'Agorinha'
-    li4.innerHTML = 'ATIVADO'
-    console.log(divList)
+      li.innerHTML = 'bitcoin'
+      li2.innerHTML = '0'
+      li3.innerHTML = 'Hoje'
+      li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'Ethereum'){
+        const divMoedas = document.querySelector(".walletBorder");
+        const divList = document.createElement("div");
+  
+        const divSeparadores = document.createElement("div");
+  
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+  
+        const li2 = document.createElement("li");
+        const divSeparadores2 = document.createElement("div");
+        const ul2 = document.createElement("ul");
+  
+        const li3 = document.createElement("li");
+        const divSeparadores3 = document.createElement("div");
+        const ul3 = document.createElement("ul");
+  
+        const li4 = document.createElement("li");
+        const divSeparadores4 = document.createElement("div");
+        const ul4 = document.createElement("ul");
+  
+  
+        divMoedas.appendChild(divList);
+  
+        divList.className = 'listaMoedas'
+        divList.appendChild(divSeparadores);
+        divList.appendChild(divSeparadores2);
+        divList.appendChild(divSeparadores3);
+        divList.appendChild(divSeparadores4);
+  
+        divSeparadores.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+  
+        divSeparadores.appendChild(ul);
+        divSeparadores2.appendChild(ul2);
+        divSeparadores3.appendChild(ul3);
+        divSeparadores4.appendChild(ul4);
+  
+        ul.className = 'walletColumn'
+        
+        ul.appendChild(li);
+        ul2.appendChild(li2);
+        ul3.appendChild(li3);
+        ul4.appendChild(li4);
+  
+        li.innerHTML = 'Ethereum'
+        li2.innerHTML = '0'
+        li3.innerHTML = 'Hoje'
+        li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'USD Coin'){
+      const divMoedas = document.querySelector(".walletBorder");
+      const divList = document.createElement("div");
 
-    var select = document.getElementById("selecionarMoeda");
-    var opcaoTexto = select.options[select.selectedIndex].text;
-    console.log(opcaoTexto); // Ferrari
-  };
+      const divSeparadores = document.createElement("div");
+
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+
+      const li2 = document.createElement("li");
+      const divSeparadores2 = document.createElement("div");
+      const ul2 = document.createElement("ul");
+
+      const li3 = document.createElement("li");
+      const divSeparadores3 = document.createElement("div");
+      const ul3 = document.createElement("ul");
+
+      const li4 = document.createElement("li");
+      const divSeparadores4 = document.createElement("div");
+      const ul4 = document.createElement("ul");
+
+
+      divMoedas.appendChild(divList);
+
+      divList.className = 'listaMoedas'
+      divList.appendChild(divSeparadores);
+      divList.appendChild(divSeparadores2);
+      divList.appendChild(divSeparadores3);
+      divList.appendChild(divSeparadores4);
+
+      divSeparadores.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+
+      divSeparadores.appendChild(ul);
+      divSeparadores2.appendChild(ul2);
+      divSeparadores3.appendChild(ul3);
+      divSeparadores4.appendChild(ul4);
+
+      ul.className = 'walletColumn'
+      
+      ul.appendChild(li);
+      ul2.appendChild(li2);
+      ul3.appendChild(li3);
+      ul4.appendChild(li4);
+
+      li.innerHTML = 'USD Coin'
+      li2.innerHTML = '0'
+      li3.innerHTML = 'Hoje'
+      li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'BNB'){
+      const divMoedas = document.querySelector(".walletBorder");
+      const divList = document.createElement("div");
+
+      const divSeparadores = document.createElement("div");
+
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+
+      const li2 = document.createElement("li");
+      const divSeparadores2 = document.createElement("div");
+      const ul2 = document.createElement("ul");
+
+      const li3 = document.createElement("li");
+      const divSeparadores3 = document.createElement("div");
+      const ul3 = document.createElement("ul");
+
+      const li4 = document.createElement("li");
+      const divSeparadores4 = document.createElement("div");
+      const ul4 = document.createElement("ul");
+
+
+      divMoedas.appendChild(divList);
+
+      divList.className = 'listaMoedas'
+      divList.appendChild(divSeparadores);
+      divList.appendChild(divSeparadores2);
+      divList.appendChild(divSeparadores3);
+      divList.appendChild(divSeparadores4);
+
+      divSeparadores.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+
+      divSeparadores.appendChild(ul);
+      divSeparadores2.appendChild(ul2);
+      divSeparadores3.appendChild(ul3);
+      divSeparadores4.appendChild(ul4);
+
+      ul.className = 'walletColumn'
+      
+      ul.appendChild(li);
+      ul2.appendChild(li2);
+      ul3.appendChild(li3);
+      ul4.appendChild(li4);
+
+      li.innerHTML = 'BNB'
+      li2.innerHTML = '0'
+      li3.innerHTML = 'Hoje'
+      li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'Polygon'){
+        const divMoedas = document.querySelector(".walletBorder");
+        const divList = document.createElement("div");
+  
+        const divSeparadores = document.createElement("div");
+  
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+  
+        const li2 = document.createElement("li");
+        const divSeparadores2 = document.createElement("div");
+        const ul2 = document.createElement("ul");
+  
+        const li3 = document.createElement("li");
+        const divSeparadores3 = document.createElement("div");
+        const ul3 = document.createElement("ul");
+  
+        const li4 = document.createElement("li");
+        const divSeparadores4 = document.createElement("div");
+        const ul4 = document.createElement("ul");
+  
+  
+        divMoedas.appendChild(divList);
+  
+        divList.className = 'listaMoedas'
+        divList.appendChild(divSeparadores);
+        divList.appendChild(divSeparadores2);
+        divList.appendChild(divSeparadores3);
+        divList.appendChild(divSeparadores4);
+  
+        divSeparadores.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+        divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+  
+        divSeparadores.appendChild(ul);
+        divSeparadores2.appendChild(ul2);
+        divSeparadores3.appendChild(ul3);
+        divSeparadores4.appendChild(ul4);
+  
+        ul.className = 'walletColumn'
+        
+        ul.appendChild(li);
+        ul2.appendChild(li2);
+        ul3.appendChild(li3);
+        ul4.appendChild(li4);
+  
+        li.innerHTML = 'Polygon'
+        li2.innerHTML = '0'
+        li3.innerHTML = 'Hoje'
+        li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'XRP'){
+      const divMoedas = document.querySelector(".walletBorder");
+      const divList = document.createElement("div");
+
+      const divSeparadores = document.createElement("div");
+
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+
+      const li2 = document.createElement("li");
+      const divSeparadores2 = document.createElement("div");
+      const ul2 = document.createElement("ul");
+
+      const li3 = document.createElement("li");
+      const divSeparadores3 = document.createElement("div");
+      const ul3 = document.createElement("ul");
+
+      const li4 = document.createElement("li");
+      const divSeparadores4 = document.createElement("div");
+      const ul4 = document.createElement("ul");
+
+
+      divMoedas.appendChild(divList);
+
+      divList.className = 'listaMoedas'
+      divList.appendChild(divSeparadores);
+      divList.appendChild(divSeparadores2);
+      divList.appendChild(divSeparadores3);
+      divList.appendChild(divSeparadores4);
+
+      divSeparadores.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+
+      divSeparadores.appendChild(ul);
+      divSeparadores2.appendChild(ul2);
+      divSeparadores3.appendChild(ul3);
+      divSeparadores4.appendChild(ul4);
+
+      ul.className = 'walletColumn'
+      
+      ul.appendChild(li);
+      ul2.appendChild(li2);
+      ul3.appendChild(li3);
+      ul4.appendChild(li4);
+
+      li.innerHTML = 'XRP'
+      li2.innerHTML = '0'
+      li3.innerHTML = 'Hoje'
+      li4.innerHTML = 'Ativado'
+      }else
+    if(opcaoTexto === 'Dogecoin'){
+      const divMoedas = document.querySelector(".walletBorder");
+      const divList = document.createElement("div");
+
+      const divSeparadores = document.createElement("div");
+
+      const ul = document.createElement("ul");
+      const li = document.createElement("li");
+
+      const li2 = document.createElement("li");
+      const divSeparadores2 = document.createElement("div");
+      const ul2 = document.createElement("ul");
+
+      const li3 = document.createElement("li");
+      const divSeparadores3 = document.createElement("div");
+      const ul3 = document.createElement("ul");
+
+      const li4 = document.createElement("li");
+      const divSeparadores4 = document.createElement("div");
+      const ul4 = document.createElement("ul");
+
+
+      divMoedas.appendChild(divList);
+
+      divList.className = 'listaMoedas'
+      divList.appendChild(divSeparadores);
+      divList.appendChild(divSeparadores2);
+      divList.appendChild(divSeparadores3);
+      divList.appendChild(divSeparadores4);
+
+      divSeparadores.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores2.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores3.classList = 'walletMoedas-separadores borderRight'
+      divSeparadores4.classList = 'walletMoedas-separadores borderRight'
+
+      divSeparadores.appendChild(ul);
+      divSeparadores2.appendChild(ul2);
+      divSeparadores3.appendChild(ul3);
+      divSeparadores4.appendChild(ul4);
+
+      ul.className = 'walletColumn'
+      
+      ul.appendChild(li);
+      ul2.appendChild(li2);
+      ul3.appendChild(li3);
+      ul4.appendChild(li4);
+
+      li.innerHTML = 'Dogecoin'
+      li2.innerHTML = '0'
+      li3.innerHTML = 'Hoje'
+      li4.innerHTML = 'Ativado'
+    }
+}
 
   return (
     <>
@@ -139,7 +452,7 @@ export const WalletList = () => {
           </div>
           <div class="walletMoedas-separadores borderRight">
             <ul class="walletColumn">
-              <li>1</li>
+              <li>0</li>
             </ul>
           </div>
           <div class="walletMoedas-separadores borderRight">
@@ -153,36 +466,6 @@ export const WalletList = () => {
             </ul>
           </div>
         </div>
-        {/*  CRIAR ISSO AQUI
-         <div class="listaMoedas">
-          <div class="walletMoedas-separadores borderRight">
-              <ul class="walletColumn">
-                <li>
-                  BitCoin
-                </li>
-              </ul>
-          </div>
-          <div class="walletMoedas-separadores borderRight">
-              <ul class="walletColumn">
-                <li>
-                  1
-                </li>
-              </ul>
-          </div><div class="walletMoedas-separadores borderRight">
-              <ul class="walletColumn">
-                <li>
-                  Hoje
-                </li>
-              </ul>
-          </div><div class="walletMoedas-separadores">
-              <ul class="walletColumn">
-                <li>
-                  ativo
-                </li>
-              </ul>
-          </div>
-        </div>
-        */}
       </div>
       <button id="button-choose" onClick={addToList}>
         Adicionar Moeda
@@ -198,10 +481,10 @@ export const WalletList = () => {
               <option value="Moeda1">{Coins[0].nome}</option>
               <option value="Moeda2">{Coins[1].nome}</option>
               <option value="Moeda3">{Coins[2].nome}</option>
-              <option value="Moeda3">{Coins[3].nome}</option>
-              <option value="Moeda3">{Coins[4].nome}</option>
-              <option value="Moeda3">{Coins[5].nome}</option>
-              <option value="Moeda3">{Coins[6].nome}</option>
+              <option value="Moeda4">{Coins[3].nome}</option>
+              <option value="Moeda5">{Coins[4].nome}</option>
+              <option value="Moeda6">{Coins[5].nome}</option>
+              <option value="Moeda7">{Coins[6].nome}</option>
             </select>
             <button onClick={CriarLista}>OK</button>
           </div>
