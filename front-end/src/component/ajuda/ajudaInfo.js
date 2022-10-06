e70fec7321ba8ef5482f2ff84de2635b1e7b7abb
felipekenpachi <47869261+felipekenpachi@users.noreply.github.com>
2022-10-06 14:59:38 -0300

    [FPS]arrumando tela de ajuda

--- front-end/src/component/ajuda/ajudaInfo.js
+++ front-end/src/component/ajuda/ajudaInfo.js
@@ -3,54 +3,52 @@ import "./ajudaInfo.css";
 export const AjudaInfo = () => {
     return (
         <>
-            
-                <section class="card-tree-info">
-                    <div class="card-info">
-                        <h1 class="h1-info">Como comprar CryptoMoeda?</h1>
-                        <div class="card-content">
-                            <p>
-                                No menu do lado esquerdo, vai ter um botao chamado Exchange,
-                                clicar nele e você vai ter acesso a compra de criptomoedas.
-                            </p>
-                        </div>
-                    </div>
 
-                    <div class="card-info">
-                        <h1 class="h1-info">Como vender CryptoMoeda?</h1>
-                        <div>
-                            <p>
-                                No menu do lado esquerdo, vai ter um botao chamado Exchange,
-                                clicar nele e você vai ter acesso a venda de suas criptomoedas.
-                            </p>
-                        </div>
+            <section class="card-tree-info">
+                <div class="card-info">
+                    <h1 class="h1-info">Como comprar CryptoMoeda?</h1>
+                    <div class="card-content">
+                        <p>
+                            No menu do lado esquerdo, vai ter um botao chamado Exchange,
+                            clicar nele e você vai ter acesso a compra de criptomoedas.
+                        </p>
                     </div>
-                </section>
-            
+                </div>
 
-        
-                <section class="card-tree-info">
-                    <div class="card-info">
-                        <h1 class="h1-info">Rentabilidade em NFT</h1>
-                        <div class="card-content">
-                            <p>
-                                Como funciona a parte financeira
-                                do NFT? Uma arte pode valer
-                                dinheiro e ter outras funções?
-                            </p>
-                        </div>
+                <div class="card-info">
+                    <h1 class="h1-info">Como vender CryptoMoeda?</h1>
+                    <div class="card-content">
+                        <p>
+                            No menu do lado esquerdo, vai ter um botao chamado Exchange,
+                            clicar nele e você vai ter acesso a venda de suas criptomoedas.
+                        </p>
                     </div>
-                    <div class="card-info">
-                        <h1 class="h1-info">Rentabilidade em NFT</h1>
-                        <div class="card-content">
-                            <p>
-                                Como funciona a parte financeira
-                                do NFT? Uma arte pode valer
-                                dinheiro e ter outras funções?
-                            </p>
-                        </div>
+                </div>
+            </section>
+
+
+
+            <section class="card-tree-info">
+                <div class="card-info">
+                    <h1 class="h1-info">Como olhar as criptomoeda que comprei?</h1>
+                    <div class="card-content">
+                        <p>
+                            No menu do lado esquerdo, vai ter um botao chamado Wallet,
+                            entrando nele vai ter o nome da moeda e a quantidade que você comprou.
+                        </p>
                     </div>
-                </section>
-      
+                </div>
+                <div class="card-info">
+                    <h1 class="h1-info">Como adicionar dinheiro para comprar criptomoeda?</h1>
+                    <div class="card-content">
+                        <p>
+                            No botão menu que fica no lado direito no superior da tela, tem
+                            a opção de adicionar R$1000,00 na sua carteira.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
 
         </>
     )
